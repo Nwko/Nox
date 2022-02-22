b42a33fd41c907dde49486c2ab9cffd9873bc6b1
Nox <83485955+Nwko@users.noreply.github.com>
2022-02-22 18:18:29 +0545

    .w.

--- uglify.sh
+++ uglify.sh
@@ -2,6 +2,6 @@
 
 # test
 echo "Uglifying... (～￣▽￣)～"
-uglifyjs public/js/main.js --compress --mangle -o public/scripts/main.min.js
-uglifyjs public/js/index.js --compress --mangle -o public/scripts/index.min.js
+uglifyjs public/js/main.js --compress --mangle -o public/js/main.min.js
+uglifyjs public/js/index.js --compress --mangle -o public/js/index.min.js
 echo "Uglified! Now they are as ugly as you! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
