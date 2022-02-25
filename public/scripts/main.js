d8c30e55498cc46fde860f99b41f826875c683c2
Nox <83485955+Nwko@users.noreply.github.com>
2022-02-25 18:28:57 +0545

    🖤or 🤍
    I like 🖤 .w.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
 $(".change").on("click", function () {
-  if ($("body").hasClass("dark")) {
-    $("body").removeClass("dark");
-    $(".change").text("OFF");
+  if ($("body").hasClass("dark-mode")) {
+    $("body").removeClass("dark-mode");
+    $(".change").text("Light Mode: 🤍");
   } else {
-    $("body").addClass("dark");
-    $(".change").text("ON");
+    $("body").addClass("dark-mode");
+    $(".change").text("Dark Mode: 🖤");
   }
 });
