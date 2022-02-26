af8e9d0c9a34fce6343147de26875c3651f720e4
Nox <83485955+Nwko@users.noreply.github.com>
2022-02-26 20:29:02 +0545

    Bye bye uglifyjs
    See ya never ( •̀ ω •́ )✧

--- uglify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# test
-echo "Uglifying... (～￣▽￣)～"
-uglifyjs public/scripts/main.js --compress --mangle -o public/scripts/main.min.js
-uglifyjs public/scripts/github.js --compress --mangle -o public/scripts/github.min.js
-uglifyjs public/scripts/dlm.js --compress --mangle -o public/scripts/dlm.min.js
-echo "Uglified! Now they are as ugly as you! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
-sleep 3
