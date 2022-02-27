edb1505d4889a6db0faf6bb6dd6a58bfea0ef934
Nox <83485955+Nwko@users.noreply.github.com>
2022-02-27 16:51:24 +0545

    DLM :D
    YA

--- public/scripts/dlm.js
+++ public/scripts/dlm.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
     $("body").toggleClass("dark-mode");
   });
 });
+
+$(".change").on("click", function () {
+  if ($("body").hasClass("dark-mode")) {
+    $("body").removeClass("dark-mode");
+    $(".change").text("Light Mode: 🤍");
+  } else {
+    $("body").addClass("dark-mode");
+    $(".change").text("Dark Mode: 🖤");
+  }
+});
--- public/scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(".change").on("click", function () {
-  if ($("body").hasClass("dark-mode")) {
-    $("body").removeClass("dark-mode");
-    $(".change").text("Light Mode: 🤍");
-  } else {
-    $("body").addClass("dark-mode");
-    $(".change").text("Dark Mode: 🖤");
-  }
-});
